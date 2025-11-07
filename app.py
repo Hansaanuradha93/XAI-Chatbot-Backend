94685af46da67e60a60389c70268ad713effd9be
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2025-11-07 23:08:12 +0530

    deploy 1.1

--- app.py
+++ app.py
@@ -514,6 +514,6 @@ def root():
 # --- Run with: uvicorn app:app --reload ---
 if __name__ == "__main__":
     import uvicorn, os
-    port = int(os.environ.get("PORT", 8000))  # ✅ Render uses $PORT
+    port = int(os.environ.get("PORT", 8000))
     print(f"🚀 Starting server on port {port}")
     uvicorn.run("app:app", host="0.0.0.0", port=port)
